adc9f26f97e024afd86fcf865065126e09df83d5
Hugh Brown <hughdbrown@yahoo.com>
2009-06-22 05:10:59 -0400

    Remove dead code; Queryset.filter() does not throw ObjectDoesNotExist

--- message/views.py
+++ message/views.py
@@ -1,27 +1,12 @@
-#import logging
-#import datetime
-#from time import strftime
-
-#from django.conf import settings
-#from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
-#from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
-#from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-#from django.template.loader import render_to_string
 
 from fivesongsdaily.message.models import Message
 
 @login_required
 def show_all(request):
-    print "show_all"
     template_name = 'messages.html'
-
-    try:
-        messages = Message.objects.filter(active=True).order_by('-created_at')[:5]
-    except ObjectDoesNotExist:
-        messages = None
-
+    messages = Message.objects.filter(active=True).order_by('-created_at')[:5]
     return render_to_response(template_name, {'messages':messages}, context_instance=RequestContext(request))
 
