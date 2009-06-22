fb24522034a890e1de07842f56ba620b0512b3eb
Hugh Brown <hughdbrown@yahoo.com>
2009-06-21 20:03:46 -0400

    fivesongs to fivesongsdaily; make work with local_settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,15 @@
 import os, logging, sys
 
 # Django settings for the fivesongs project.
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+try:
+    from local_settings import *
+except ImportError:
+    try:
+        from mod_python import apache
+        apache.log_error( "local_settings.py not set; using default settings", apache.APLOG_NOTICE )
+    except ImportError:
+        import sys
+        sys.stderr.write( "local_settings.py not set; using default settings\n" )
 
 ADMINS = (
     ('', ''),
@@ -16,13 +22,6 @@ EMAIL_HOST = ''
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
 TIME_ZONE = 'America/Los Angeles'
 
 LANGUAGE_CODE = 'en-us'
@@ -31,10 +30,11 @@ SITE_ID = 1
 
 USE_I18N = False
 
-MEDIA_ROOT = '/fivesongs/site_media/'
+import django
+DJANGO_ROOT = django.__path__[0]
 
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'site_media/')
 MEDIA_URL = '/site_media/'
-
 ADMIN_MEDIA_PREFIX = '/media/'
 
 SECRET_KEY = ''
@@ -42,23 +42,23 @@ SECRET_KEY = ''
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'middleware.project_logging.LoggingMiddleware',
+    #'middleware.project_logging.LoggingMiddleware',
 )
 
-ROOT_URLCONF = 'fivesongs.urls'
+ROOT_URLCONF = 'fivesongsdaily.urls'
 
 TEMPLATE_ROOT = ''
 
 TEMPLATE_DIRS = (
-    '/fivesongs/templates/',
-    '/django/contrib/admin/templates/',
+    #'/fivesongs/templates/',
+    os.path.join(PROJECT_ROOT, 'templates/'),
+    os.path.join(DJANGO_ROOT, 'contrib/admin/templates/')
 )
 
 INSTALLED_APPS = (
@@ -68,23 +68,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.comments',
-    'fivesongs.playlist',
-    'fivesongs.profiles',
-    'fivesongs.pages',
-    'fivesongs.contact',
+    
+    'fivesongsdaily.playlist',
+    'fivesongsdaily.profiles',
+    'fivesongsdaily.pages',
+    'fivesongsdaily.contact',
+    'fivesongsdaily.message',
 )
 
-LOGOUT_URL = '/accounts/login/'
-
-AKISMET_API_KEY = ''
-
-try:
-    from local_settings import *
-except ImportError:
-    try:
-        from mod_python import apache
-        apache.log_error( "local_settings.py not set; using default settings", apache.APLOG_NOTICE )
-    except ImportError:
-        import sys
-        sys.stderr.write( "local_settings.py not set; using default settings\n" )
 
